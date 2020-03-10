58f4d5118d510ceaeb7a3ac5b9e37c243f595a27
Seetaram Yadav <in-seetaram.yadav@Seetarams-MacBook-Pro-2.local>
2020-03-10 22:57:17 +0530

    Added the test for adding one product.

--- src/cart.test.js
+++ src/cart.test.js
@@ -1,12 +1,11 @@
 import Cart from "./Cart";
 
 describe("Cart", () => {
-  it("Should add 5 Dove Soaps and 3 Dove Soaps into cart", () => {
+  it("Should And a product, Dove Soap with a unit price of 39.99", () => {
     const cart = new Cart();
 
-    cart.addProduct({ name: "Dove Soap", price: 39.99, quantity: 5 });
-    cart.addProduct({ name: "Dove Soap", price: 39.99, quantity: 3 });
-    expect(cart.getProdcutCount()).toBe(8);
-    expect(cart.getTotalPrice()).toBe(319.92);
+    cart.addProduct({ name: "Dove Soap", price: 39.99, quantity: 1 });
+    expect(cart.getProdcutCount()).toBe(1);
+    expect(cart.getTotalPrice()).toBe(39.99);
   });
 });

63dbde793fd86674ac36af50f3e27b3898f6b118
Seetaram Yadav <in-seetaram.yadav@Seetarams-MacBook-Pro-2.local>
2020-03-10 23:06:05 +0530

    Added test for calculating tax.

--- src/cart.test.js
+++ src/cart.test.js
@@ -8,4 +8,15 @@ describe("Cart", () => {
     expect(cart.getProdcutCount()).toBe(1);
     expect(cart.getTotalPrice()).toBe(39.99);
   });
+
+  it("should add a product Dove Soap with a unit price and another product, Axe Deo with a unit price", () => {
+    const cart = new Cart();
+
+    cart.addProduct({ name: "Dove Soap", price: 39.99, quantity: 2 });
+    cart.addProduct({ name: "Axe Deos", price: 99.99, quantity: 2 });
+
+    expect(cart.getProdcutCount()).toBe(4);
+    expect(cart.totalSalesTaxAmount()).toBe(35.0);
+    expect(cart.getTotalPrice()).toBe(314.96);
+  });
 });
